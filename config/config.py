5703427aac37085e822c06b1fae71e3b86a15519
Om Mishra <ommishra052072@gmail.com>
2026-07-11 13:31:44 +0530

    Changed config

--- config/config.py
+++ config/config.py
@@ -1,3 +1,15 @@
+FACE_BLUR = True
+
+OBJECT_BLUR = True
+
+OCR_ENABLED = True
+
+YOLO_CONFIDENCE = 0.5
+
+FACE_CONFIDENCE = 0.5
+
+OCR_INTERVAL = 15
+
 SENSITIVE_OBJECTS = {
     "cell phone",
     "laptop",
